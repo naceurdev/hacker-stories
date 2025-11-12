7f6c9432713467a4133b6f5e53a15b655a8faa21
Naceur <naceurbenyahia1@gmail.com>
2025-11-12 16:22:05 +0100

    decompose to many components and add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
-const name = 'React';
 
-function App() {
+const Item = ({ item }) =>
+(<li>
+  <span>{item.title}</span>
+  <span>{item.url}</span>
+  <span>{item.author}</span>
+  <span>{item.num_comments}</span>
+  <span>{item.points}</span>
+</li>
+)
+
+
+const List = ({ list }) =>
+  <ul>
+    {list.map((item) => (
+      <Item item={item} key={item.objectID } />
+    ))}
+  </ul>;
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+
+    console.log(event.target.value);
+  }
+
+  const handleBlur = () => {
+    console.log('Input lost focus');
+  }
+
   return (
     <div>
-      <h1>Hello {name}</h1>
+      <label htmlFor="inputField">Input Field</label>
+      <input id="inputField" type="text" onBlur={handleBlur} onChange={handleChange} />
     </div>
   )
-}
+};
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.dev/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: '0',
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: '1',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
+  )
+};
 
 export default App
